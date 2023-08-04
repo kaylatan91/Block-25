4ace124bf099191f58eb7bc7e0d97163a70e4e8e
Annalisa Boerner <annalisa.boerner@gmail.com>
2023-08-04 14:22:13 -0400

    pastel stoplight version

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 // Write your Color component here
+const Color = (props) => {
+     return <div className={props.color}></div>;
+};
 
 const App = () => {
-  return (
-    <div id="container">
-      <div id="navbar">
-        <div>Currently selected: </div>
-        <div className="red">red</div>
-      </div>
-      <div id="colors-list">{/* colors go here */}</div>
-    </div>
-  );
+     return (
+          <div id="container">
+               <div id="navbar">
+                    <div>Currently selected: </div>
+                    <div className="red">red</div>
+               </div>
+               <div id="colors-list">
+                    <Color color="orange" />
+                    <Color color="yellow" />
+                    <Color color="green" />
+               </div>
+          </div>
+     );
 };
 
 export default App;
